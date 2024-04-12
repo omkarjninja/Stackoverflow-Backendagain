b14ae4851c8654c0dfb65f95e01dddfa1fa80319
Omkar j <107001176+omkarjninja@users.noreply.github.com>
2024-04-13 02:04:29 +0530

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,7 +13,7 @@ export const signup = async (req, res) => {
 
         const hashedPassword = await bcrypt.hash(password, 12)
         const newUser = await users.create({ name,email,password: hashedPassword })
-        const token = jwt.sign({ email: newUser.email, id:newUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ email: newUser.email, id:newUser._id }, process.env.JWT_SECRET, { expiresIn: '365d' });
         res.status(200).json({ result: newUser, token })
     } catch(error){
         res.status(500).json("Something went wrong...")
@@ -33,7 +33,7 @@ export const login = async (req, res) => {
         if(!isPasswordCrt){
             return res.status(400).json({message : "Invalid credentials"})
         } 
-        const token = jwt.sign({ email: existinguser.email, id:existinguser._id }, process.env.JWT_SECRET , { expiresIn: '1h' });
+        const token = jwt.sign({ email: existinguser.email, id:existinguser._id }, process.env.JWT_SECRET , { expiresIn: '365d' });
         res.status(200).json({ result: existinguser, token })
 
     } catch (error) {
